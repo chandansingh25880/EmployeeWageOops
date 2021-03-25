2f5898b07feaf540547fb1260cc1282ec8582770
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 21:38:11 +0530

    Added: Solving Using Switch Case Statement

--- src/com/wage/EmployeeWageOop.java
+++ src/com/wage/EmployeeWageOop.java
@@ -1,16 +1,16 @@
 package com.wage;
+
 public class EmployeeWageOop {
 	// Creating a method
 	public void welcomeMsg() {
 		// Check the employee is present or absent
-		int Is_Part_Time = 1; // Added part time condition
-		int IS_FULL_TIME = 2;
+		final int Is_Part_Time = 1; // Added part time condition
+		final int IS_FULL_TIME = 2;
 		// Calculating daily employee wage
 		final int empWagePerHrs = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		 // Check random value 
-		double empcheck = Math.floor(Math.random() * 10) % 3; 
+		double empcheck = Math.floor(Math.random() * 10) % 3; // Check random value between 0,1 and 2
 		if (empcheck == Is_Part_Time) {
 			empHrs = 8;
 		} else if (empcheck == IS_FULL_TIME) {
@@ -19,6 +19,21 @@ public class EmployeeWageOop {
 			empHrs = 0;
 		empWage = empHrs * empWagePerHrs;
 		System.out.println("EmpWage :" + " " + empWage);
+
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case Is_Part_Time:
+			empHrs = 4;
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
+			empHrs = 0;
+		}
+		empWage = empHrs * empHrs;
+		System.out.println("Emp Wage" + " " + empWage);
+
 	}
 
 	public static void main(String[] args) {
