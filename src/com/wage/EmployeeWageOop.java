f84d3977066c853504bb8fcc61e61de98d254cd6
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 21:09:51 +0530

    Added: Employee Attendance

--- src/com/wage/EmployeeWageOop.java
+++ src/com/wage/EmployeeWageOop.java
@@ -1,7 +1,27 @@
 package com.wage;
 public class EmployeeWageOop {
-	public static void main(String[] args) {
-		System.out.println("Welocme to the Employee Wages Computation");
-	}
+			// Creating a method
+			public void welcomeMsg() {
+				System.out.println("Welcome to employee wage Details");
 
-}
+				// Check the employee is present or absent
+				int IS_FULL_TIME = 1;
+				double empcheck = Math.floor(Math.random() * 10) % 2;
+				if (empcheck == IS_FULL_TIME) {
+					System.out.println("emp is present");
+				} else {
+					System.out.println("emp is not present");
+				}
+
+			}
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				// To creating a object and call the method
+				EmployeeWageOop obj = new EmployeeWageOop();
+				obj.welcomeMsg();
+
+			}
+			
+		}
+	
