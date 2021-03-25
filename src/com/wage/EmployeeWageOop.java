ab381d6f6d29067e1590c2f3262fea3e1a8d39ff
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 22:08:11 +0530

    Added: To Calculate Wages for a month

--- src/com/wage/EmployeeWageOop.java
+++ src/com/wage/EmployeeWageOop.java
@@ -1,11 +1,14 @@
 package com.wage;
 
 public class EmployeeWageOop {
+
 	// Creating a method
 	public void welcomeMsg() {
 		// Check the employee is present or absent
 		final int Is_Part_Time = 1; // Added part time condition
 		final int IS_FULL_TIME = 2;
+		
+	    final int NoOfWorkingDays = 2;
 		// Calculating daily employee wage
 		final int empWagePerHrs = 20;
 		int empHrs = 0;
@@ -19,28 +22,62 @@ public class EmployeeWageOop {
 			empHrs = 0;
 		empWage = empHrs * empWagePerHrs;
 		System.out.println("EmpWage :" + " " + empWage);
-
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		
+		int empCheck=(int)Math.floor(Math.random() *10) %3 ;
 		switch (empCheck) {
 		case Is_Part_Time:
-			empHrs = 4;
+			empHrs=4;
 			break;
 		case IS_FULL_TIME:
-			empHrs = 8;
+			empHrs=8;
 			break;
 		default:
-			empHrs = 0;
+			empHrs=0;
 		}
-		empWage = empHrs * empHrs;
-		System.out.println("Emp Wage" + " " + empWage);
-
+		empWage=empHrs*empHrs;
+		System.out.println("Emp Wage"+ " "+ empWage);
+		
 	}
+	public void empWage() {
+		 final int IsPartTime = 1;
+		 final int IsFullTime = 2;
+		 final int EmpRatePerHrs = 20;
+		 final int NoOfWorkingDays = 2;
+		 
+		 int EmpWage = 0;
+			int Emphrs = 0;
+			int totalEmpWage = 0;
+			for (int days = 0; days < NoOfWorkingDays; days++) {
+				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				switch (empCheck) {
+				case IsPartTime:
+					Emphrs = 4;
+					break;
+				case IsFullTime:
+					Emphrs = 8;
+					break;
+				default:
+					Emphrs = 0;
+				}
+				EmpWage = Emphrs * EmpRatePerHrs;
+				totalEmpWage += EmpWage;
+				System.out.println(" ");
+				System.out.println("Emp Wage" + " " + EmpWage);
+			}
+			System.out.println("totalEmpWage" + " " + totalEmpWage);
+
+
+		
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// To creating a object and call the method
-		EmployeeWageOop obj = new EmployeeWageOop();
-		obj.welcomeMsg();
+		EmployeeWageOop obj1 = new EmployeeWageOop();
+		obj1.welcomeMsg();
+	     EmployeeWageOop obj2 = new EmployeeWageOop();
+		obj2.empWage();
 
 	}
 
