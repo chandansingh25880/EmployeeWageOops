8d84135bead97c36cc9c7b849207203bb09f8860
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 21:30:42 +0530

    Added: Add part time Employee Wages

--- src/com/wage/EmployeeWageOop.java
+++ src/com/wage/EmployeeWageOop.java
@@ -1,29 +1,24 @@
 package com.wage;
-
 public class EmployeeWageOop {
 	// Creating a method
 	public void welcomeMsg() {
-
 		// Check the employee is present or absent
-		int IS_FULL_TIME = 1;
+		int Is_Part_Time = 1; // Added part time condition
+		int IS_FULL_TIME = 2;
 		// Calculating daily employee wage
 		final int empWagePerHrs = 20;
 		int empHrs = 0;
 		int empWage = 0;
-
-		double empcheck = Math.floor(Math.random() * 10) % 2;
-		if (empcheck == IS_FULL_TIME) {
-
+		 // Check random value 
+		double empcheck = Math.floor(Math.random() * 10) % 3; 
+		if (empcheck == Is_Part_Time) {
 			empHrs = 8;
-			System.out.println("emp is present");
-		} else {
-
+		} else if (empcheck == IS_FULL_TIME) {
+			empHrs = 4; //
+		} else
 			empHrs = 0;
-			System.out.println("emp is not present");
-		}
 		empWage = empHrs * empWagePerHrs;
 		System.out.println("EmpWage :" + " " + empWage);
-
 	}
 
 	public static void main(String[] args) {
