154506081a60873c89c8601abf63916b35c63742
chandansingh25880 <chandan.1sep1997@gmail.com>
2021-03-25 21:18:13 +0530

    Added: To Calculate Daily Employee Wages

--- src/com/wage/EmployeeWageOop.java
+++ src/com/wage/EmployeeWageOop.java
@@ -1,27 +1,37 @@
 package com.wage;
+
 public class EmployeeWageOop {
-			// Creating a method
-			public void welcomeMsg() {
-				System.out.println("Welcome to employee wage Details");
+	// Creating a method
+	public void welcomeMsg() {
 
-				// Check the employee is present or absent
-				int IS_FULL_TIME = 1;
-				double empcheck = Math.floor(Math.random() * 10) % 2;
-				if (empcheck == IS_FULL_TIME) {
-					System.out.println("emp is present");
-				} else {
-					System.out.println("emp is not present");
-				}
+		// Check the employee is present or absent
+		int IS_FULL_TIME = 1;
+		// Calculating daily employee wage
+		final int empWagePerHrs = 20;
+		int empHrs = 0;
+		int empWage = 0;
 
-			}
+		double empcheck = Math.floor(Math.random() * 10) % 2;
+		if (empcheck == IS_FULL_TIME) {
 
-			public static void main(String[] args) {
-				// TODO Auto-generated method stub
-				// To creating a object and call the method
-				EmployeeWageOop obj = new EmployeeWageOop();
-				obj.welcomeMsg();
+			empHrs = 8;
+			System.out.println("emp is present");
+		} else {
 
-			}
-			
+			empHrs = 0;
+			System.out.println("emp is not present");
 		}
-	
+		empWage = empHrs * empWagePerHrs;
+		System.out.println("EmpWage :" + " " + empWage);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// To creating a object and call the method
+		EmployeeWageOop obj = new EmployeeWageOop();
+		obj.welcomeMsg();
+
+	}
+
+}
